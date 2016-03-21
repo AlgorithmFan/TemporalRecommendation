34c2dbe0a16605186c78f29d642fb4981e1c2a2d
zhang.haidong <haidong_zhang13@163.com>
2016-03-21 23:08:42 +0800

    init

---
+++ TDCF/TDCF.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+#coding:utf-8
+
+'''
+Temporal decaying for RS
+'''
+
+class TDCF:
+    def __init__(self, trainMatrix, testMatrix, configHandler):
+        self.trainMatrix = trainMatrix
+        self.testMatrix = testMatrix
+        self.configHandler = configHandler
+
+
+
